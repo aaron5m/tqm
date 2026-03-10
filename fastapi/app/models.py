85fe05cd08b96bf21e542d2e00525021f53b2c46
Aaron Mitchell <me@paaronmitchell.com>
2026-03-10 11:50:22 +0700

    getting alembic working

---
+++ fastapi/app/models.py
@@ -0,0 +1,16 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+from typing import Optional, List
+from sqlalchemy import Column, JSON
+
+class Link(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    url: str
+    description: Optional[str] = None
+    # Use default_factory so the timestamp is generated at the moment of creation
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    upvotes: int = Field(default=0)
+    
+    # For simplicity in prototyping, we use a JSON-type column for photo URLs
+    photos: Optional[List[str]] = Field(default=[], sa_column=Column(JSON))
